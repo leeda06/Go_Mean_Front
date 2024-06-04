507fc159a3bc3ba70202733849c9c96ff9fa1c1e
leeda06 <slmos33a@gmail.com>
2024-06-04 14:31:21 +0900

    :tada: [feat] : 프로젝트 시작

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const App = () => {
+  return (
+    <React.StrictMode>
+      <div></div>
+    </React.StrictMode>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
