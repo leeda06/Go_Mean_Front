c367f1282db98bc492370caa7a15287fa44d8919
leeda06 <slmos33a@gmail.com>
2024-06-04 14:54:00 +0900

    :sparkles: [feat] : index 화면 nav - js와 css코드 삽입 - 클릭시 해당 화면으로 이동(임시)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,39 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './css/index.css';
+
+import Record from './commpnent/record/record';
+import Graph from './commpnent/graph/graph';
 
 const App = () => {
+  const [activeComponent, setActiveComponent] = useState('Graph');
+
+  const handleButtonClick = (component) => {
+    setActiveComponent(component);
+  };
+
   return (
     <React.StrictMode>
-      <div></div>
+      {activeComponent === 'Record' ? <Record /> : <Graph />}
+      <nav>
+        <div className="Button-back">
+          <button className={`slide-btn`}>홈</button>
+          <button
+            className={`slide-btn Trash-btn ${activeComponent === 'Record' ? 'active' : ''}`}
+            style={{ backgroundColor: `${activeComponent === 'Record' ? '#ffffff50' : ''}` }}
+            onClick={() => handleButtonClick('Record')}
+          >
+            쓰레기 보관함
+          </button>
+          <button
+            className={`slide-btn Statistics-btn ${activeComponent === 'Graph' ? 'active' : ''}`}
+            style={{ backgroundColor: `${activeComponent === 'Graph' ? '#ffffff50' : ''}` }}
+            onClick={() => handleButtonClick('Graph')}
+          >
+            통계
+          </button>
+        </div>
+      </nav>
     </React.StrictMode>
   );
 };
