fd80db3ff68d0809322eac95e7548e9c32cb6aa4
seoungju0828 <ministar2006@gmail.com>
2024-06-10 13:46:43 +0900

    CSS 변경

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
 // 홈화면 렌더링
-import Home from './commpnent/home/home.js';
+import Home from './components/home/home.js';
 
 // 고민작성 화면 렌더링
-import Write from './commpnent/write/write.js';
+import Write from './components/write/write.js';
 
 // 통계 화면 렌더링
-import Graph from './commpnent/graph/graph.js';
+import Graph from './components/graph/graph.js';
 
 // 쓰레기보관함 화면 렌더링
-import Record from './commpnent/record/record.js';
+import Record from './components/record/record.js';
 
 const App = () => {
     const [activeComponent, setActiveComponent] = useState('Home');
