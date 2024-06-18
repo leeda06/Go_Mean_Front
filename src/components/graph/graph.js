f62caeb4c12c23940dea52b5a5ca31b3b5e97b46
haeunNoh06 <okhagrace06@naver.com>
2024-06-18 14:56:01 +0900

    :recycle: [refactor] 배포된 서버 url, category id 반영

--- src/components/graph/graph.js
+++ src/components/graph/graph.js
@@ -5,12 +5,12 @@ import axios from 'axios';
 
 // `legendItems` array containing gif and color information
 const legendItems = [
-    { category_id: "666d423e86329950249cafb9", gif: require('../../img/gif/Substract_green.gif'), category: '건강', color: '연두색', className: 'green' },
-    { category_id: "666d424286329950249cafba", gif: require('../../img/gif/Substract_yellow.gif'), category: '금전', color: '노랑색', className: 'yellow' },
-    { category_id: "666d424586329950249cafbb", gif: require('../../img/gif/Substract_white.gif'), category: '개인', color: '흰색', className: 'white' },
-    { category_id: "666d424a86329950249cafbc", gif: require('../../img/gif/Substract_pink.gif'), category: '인간관계', color: '분홍색', className: 'pink' },
-    { category_id: "666d424f86329950249cafbd", gif: require('../../img/gif/Substract_purple.gif'), category: '취업', color: '보라색', className: 'purple' },
-    { category_id: "666d425186329950249cafbe", gif: require('../../img/gif/Substract_blue.gif'), category: '학업', color: '파란색', className: 'blue' }
+    { category_id: "667114bdf81f1fd0f6a26a25", gif: require('../../img/gif/Substract_green.gif'), category: '건강', color: '연두색', className: 'green' },
+    { category_id: "667114bdf81f1fd0f6a26a26", gif: require('../../img/gif/Substract_yellow.gif'), category: '금전', color: '노랑색', className: 'yellow' },
+    { category_id: "667114bdf81f1fd0f6a26a27", gif: require('../../img/gif/Substract_white.gif'), category: '개인', color: '흰색', className: 'white' },
+    { category_id: "667114bdf81f1fd0f6a26a28", gif: require('../../img/gif/Substract_pink.gif'), category: '인간관계', color: '분홍색', className: 'pink' },
+    { category_id: "667114bdf81f1fd0f6a26a29", gif: require('../../img/gif/Substract_purple.gif'), category: '취업', color: '보라색', className: 'purple' },
+    { category_id: "667114bdf81f1fd0f6a26a2a", gif: require('../../img/gif/Substract_blue.gif'), category: '학업', color: '파란색', className: 'blue' }
 ];
 
 const App = () => {
@@ -19,7 +19,7 @@ const App = () => {
     useEffect(() => {
         const fetchWorries = async () => {
             try {
-                const response = await axios.get('http://localhost:5000/worries');
+                const response = await axios.get(`${process.env.REACT_APP_SERVER}/worries`);
                 const fetchedWorries = response.data;
 
                 const counts = legendItems.map(item => ({
